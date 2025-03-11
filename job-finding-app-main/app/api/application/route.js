63923c2fd9150f06f317552381d002cbbded998e
Sharonrosesiyanata <sharonrosesiyanata7@gmail.com>
2025-03-11 10:05:45 +0200

    commit

--- job-finding-app-main/app/api/application/route.js
+++ job-finding-app-main/app/api/application/route.js
@@ -1,12 +1,12 @@
 import { prisma } from "../../db";
 import { NextResponse } from "next/server";
 
-export async function GET(req) {
-  const applications = await prisma.application.findMany();
-  console.log("GET applications called");
+// export async function GET(req) {
+//   const applications = await prisma.application.findMany();
+//   console.log("GET applications called");
 
-  return NextResponse.json(applications);
-}
+//   return NextResponse.json(applications);
+// }
 
 export async function POST(req) {
   const { name, address, occupation, bio } = await req.json();

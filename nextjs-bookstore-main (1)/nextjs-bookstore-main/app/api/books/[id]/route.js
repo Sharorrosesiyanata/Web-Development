9366a8060ea0f4c65e44dac24ab7fdc9c5630916
Sharonrosesiyanata <sharonrosesiyanata7@gmail.com>
2025-03-18 10:25:17 +0200

    commit

--- nextjs-bookstore-main (1)/nextjs-bookstore-main/app/api/books/[id]/route.js
+++
@@ -1,9 +0,0 @@
-import {NextResponse} from 'next/server';
-
-export const DELETE = async(request, {params}) =>{
-    const id = params.id;
-
-    await prisma.book.delete({where: {id:id}})
-
-    return new NextResponse({"Book deleted": id});
-}
--- nextjs-bookstore-main (1)/nextjs-bookstore-main/app/loading.jsx
+++
@@ -1,8 +0,0 @@
-const LoadingPage = () => {
-    return (
-        <div>
-            <button className="btn loading">loading</button>
-        </div>
-    )
-}
-export default LoadingPage;

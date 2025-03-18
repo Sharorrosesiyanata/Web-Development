b41d0b54673646548f174c449d0c8f4ae8cf21a7
Sharonrosesiyanata <sharonrosesiyanata7@gmail.com>
2025-03-18 10:42:23 +0200

    commit

--- Project Employment-Platform-app-main/app/api/application/[id]/route.js
+++
@@ -1,8 +0,0 @@
-import { NextResponse } from "next/server";
-
-export async function DELETE(request, {params}) {
-    const id = params.id;
-    await prisma.quote.delete({where: {id: id}});
-    
-    return new NextResponse({"Book deleted": id});
-}
--- Project Employment-Platform-app-main/app/api/hello/route.js
+++
@@ -1,7 +0,0 @@
-export async function GET(req) {
-    return new Response("Hello World!");
-};
-
-// export async function POST(req) {
-//     return new Response("Hello World!");
-// };

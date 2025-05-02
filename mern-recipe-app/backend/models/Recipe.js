220aeac749774a1b159bc6dbe5817876fa4676b9
Sharonrosesiyanata <sharonrosesiyanata7@gmail.com>
2025-05-02 14:28:23 +0200

    commit

--- mern-recipe-app/backend/models/Recipe.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require("mongoose");
-
-const recipeSchema = new mongoose.Schema({
-  name: String,
-  ingredients: String,
-  userId: String,
-});
-
-module.exports = mongoose.model("Recipe", recipeSchema);
